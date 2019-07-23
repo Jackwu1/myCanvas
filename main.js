d0271c10ed66569978605b5aae178dcdca34f727
wuzhenquan <7827354@qq.com>
2019-07-23 15:51:12 +0800

    我的画板

--- main.js
+++ main.js
@@ -4,19 +4,35 @@ let endX = null;
 let endY = null;
 let onClick = false;//鼠标是否按下
 let drawOrClean = 'draw';
-
+let lineWidth = 10
 let canvas = document.getElementById('canvas');
 let btn = document.getElementById('btn');
 let eraser = document.getElementById('eraser');
 let remove = document.getElementById('remove');
 let ctx = canvas.getContext('2d');
 let color = document.getElementsByClassName('color');
-let download = document.getElementById('download')
+let download = document.getElementById('download');
+let line = document.getElementsByClassName('line');
+
 getClient();
 window.onresize = function() {
 	getClient()
 }
-
+// 画笔粗细切换
+for(let i = 0; i < line.length; i++) {
+	line[i].onclick = function() {
+		for(let j = 0; j < line.length; j++)  {
+			line[j].className = 'line';
+		}
+		line[i].className = 'line active';
+		if(i === 0) {
+			lineWidth = 10;
+		}else{
+			lineWidth = 5
+		}
+	}
+}
+// 画笔颜色切换
 for(let i = 0; i < color.length; i++) {
 	color[i].onclick = function() {
 		for(let j = 0; j < color.length; j++) {
@@ -26,27 +42,31 @@ for(let i = 0; i < color.length; i++) {
 		ctx.strokeStyle  = color[i].id;
 	}
 }
+
 //得到当前设备的宽和gao
 function getClient() {
 	let pageWidth = document.documentElement.clientWidth;
 	let pageHeight = document.documentElement.clientHeight;
 	canvas.width = pageWidth;
 	canvas.height = pageHeight;
+	
 }
-canvas.onmousedown = function(e) {
+// 点击事件
+function clickEvent(e, pageX, pageY) {
 	onClick = true;
 	if(onClick === true){
 		ctx.beginPath();
-		startX = e.pageX;
-		startY = e.pageY;
+		startX = pageX;
+		startY = pageY;
 		ctx.moveTo(startX, startY);
 	}
 }
-
-canvas.onmousemove = function(e) {
+// 滑动事件
+function slide(e, pageX, pageY) {
+	ctx.lineWidth = lineWidth;
 	if(onClick && drawOrClean === 'draw'){
-		endX = e.pageX;
-		endY = e.pageY;
+		endX = pageX;
+		endY = pageY;
 		ctx.lineTo(endX, endY);
 		ctx.stroke();
 		startX = endX;
@@ -55,9 +75,34 @@ canvas.onmousemove = function(e) {
 		ctx.clearRect(e.pageX-5, e.pageY-5, 10, 10)
 	}	
 }
-canvas.onmouseup = function(e) {
+// 鼠标移开事件
+function withdraw() {
 	onClick = false;
 }
+if(document.body.ontouchstart === undefined) {
+	canvas.onmousedown = function(e) {
+		console.log(e,'eeeee')
+		clickEvent(e, e.pageX, e.pageY)
+	}
+	canvas.onmousemove = function(e) {
+		slide(e, e.pageX, e.pageY);
+	}
+	canvas.onmouseup = function(e) {
+		withdraw(e);
+	}
+}else {
+	canvas.ontouchstart = function(e) {
+		console.log(e,'eeeee')
+		clickEvent(e, e.touches[0].pageX, e.touches[0].pageY)
+	}
+	canvas.ontouchmove = function(e) {
+		slide(e, e.touches[0].pageX, e.touches[0].pageY);
+	}
+	canvas.ontouchend = function(e) {
+		withdraw(e);
+	}
+}
+
 
 btn.onclick = function() {
 	drawOrClean = 'draw';
